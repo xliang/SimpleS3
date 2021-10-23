2cf520343bd0d2d7cd59b5fdf7952f0ae0e86fea
Ian Qvist <qvistian@gmail.com>
2021-10-24 01:45:23 +0200

    Create a ThrowIfNotSuccess extension on responses

---
+++ src/SimpleS3.Core/Extensions/ResponseExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Genbox.SimpleS3.Core.Abstracts.Response;
+using Genbox.SimpleS3.Core.Common.Exceptions;
+
+namespace Genbox.SimpleS3.Core.Extensions
+{
+    public static class ResponseExtensions
+    {
+        public static void ThrowIfNotSuccess(this IResponse response)
+        {
+            if (!response.IsSuccess)
+                throw new S3ResponseException(response);
+        }
+    }
+}
